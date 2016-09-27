ab5f9a5fec471e7f05a73814b3aca1bc5555a72e
bdangit <me@bdang.it>
2016-09-26 22:48:13 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# depot
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'depot'
+maintainer 'Ben Dang'
+maintainer_email 'me@bdang.it'
+license 'mit'
+description 'Installs/Configures depot'
+long_description 'Installs/Configures depot'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/depot/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/depot' if respond_to?(:source_url)

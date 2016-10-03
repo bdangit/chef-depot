3702f887e956535755726d99e38efff617b94b23
bdangit <me@bdang.it>
2016-10-02 23:44:55 -0700

    correct source links

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'me@bdang.it'
 license 'mit'
 description 'Installs/Configures Habitat Depot'
 long_description 'Installs/Configures Habitat Depot'
-version '0.1.2'
+version '0.1.3'
 
 depends 'habitat'
 depends 'systemd'
 
-issues_url 'https://github.com/bdangit/depot/issues' if respond_to?(:issues_url)
-source_url 'https://github.com/bdangit/depot' if respond_to?(:source_url)
+issues_url 'https://github.com/bdangit/chef-depot/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/bdangit/chef-depot' if respond_to?(:source_url)
